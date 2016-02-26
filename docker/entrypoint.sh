2e6ab94389d771ce10373272f13ac6ec096db7ec
Delermando <delsantos@hotmail.com.br>
2016-02-25 22:31:14 -0300

    change folder permissions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+mkdir -p /var/www/html/www/admin/plugins
 chown -R www-data:www-data .
+chmod -R a+w /var/www/html/plugins
+chmod -R a+w /var/www/html/www/admin/plugins
+
 /etc/init.d/php5-fpm start
 nginx -g 'daemon off;'
