c3c7a6108f2767c1b5c488a9f303d33f14c1cb8a
Ian Campbell <ipcampbell@gmail.com>
2017-12-14 18:07:35 -0500

    Remove app directory.

--- app/var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2016-02-25 21:47:34
-         compiled from layout/content-footer.html */ ?>
-<?php if ($this->_tpl_vars['showContentFrame']): ?>
-</div>
-<?php endif; ?>
